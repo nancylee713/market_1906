13e49b79216ff69adc44cae0659fa276b319bd91
Nancy Lee <srlee713@gmail.com>
2019-07-24 10:49:46 -0600

    Complete Iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -18,4 +18,21 @@ class Market
   def vendors_that_sell(fruit)
     @vendors.select { |vendor| vendor.inventory.has_key? fruit }
   end
+
+  def sorted_item_list
+    @vendors.map(&:inventory).map(&:keys).flatten.uniq.sort
+  end
+
+  def total_inventory
+    total = Hash.new(0)
+    @vendors.map(&:inventory)
+      .each do |item|
+        if total.has_key? item.keys[0]
+          total[item.keys[0]] += item[item.keys[0]]
+        else
+          total.merge! item
+        end
+      end
+    total
+  end
 end
